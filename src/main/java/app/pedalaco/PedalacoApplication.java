54849a361e6c944f24788136e51d8b7d316c0065
SOekd <augustorech.contato@gmail.com>
2023-09-13 21:38:48 -0300

    first commit

---
+++ src/main/java/app/pedalaco/PedalacoApplication.java
@@ -0,0 +1,17 @@
+package app.pedalaco;
+
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@PWA(name = "Pedalaco", shortName = "Pedalaco")
+@Theme("my-theme")
+public class PedalacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedalacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/pedalaco/PedalacoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.pedalaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedalacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
